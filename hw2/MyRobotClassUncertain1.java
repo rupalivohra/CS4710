b7b8d7f903d71c9a13f44ef87d3898cdbd519131
davidamin <amin.david.m@gmail.com>
2015-03-05 01:52:43 -0500

    Not working or fully implemented
    Needs to implement poll, but more importantly, there's an issue where
    the prevNodes are being set wrong and a loop is forming. Will address
    soon.

--- hw2/MyRobotClassUncertain1.java
+++ hw2/MyRobotClassUncertain1.java
@@ -133,35 +133,67 @@ public class MyRobotClassUncertain1 extends Robot {
 	            for(int i = 0; i < moveList.size(); i++){
 	                temp_p = new Point(moveList.get(i).getX(),moveList.get(i).getY());
 	                super.move(temp_p);
+	                nodeMap.get(super.getPosition()).setVisited();
 	                if(super.getPosition().equals(temp_p)){
 	                    known.put(temp_p, "O");
 	                }else{//If we get to the node or we hit an unexpected barrier, empty queue, add current position and start over
+	                    known.put(temp_p, "X");
 	                    start_node = nodeMap.get(temp_p);
 	                    cur_node = nodeMap.get(temp_p);
+	                    q.clear();
+	                    q.add(start_node);
+	                    break;
+	                }
+	                if(super.getPosition().equals(new Point(moveList.get(moveList.size() - 1).getX(),moveList.get(moveList.size() - 1).getY()))){
+	                    System.out.println("GOT TO TARGET");
+	                    start_node = nodeMap.get(super.getPosition());
+	                    cur_node = nodeMap.get(super.getPosition());
 	                    q.clear();
 	                    q.add(start_node);
 	                }
 	            }
 	        }else{
+	            //System.out.println(cur_node);
 	            //Look for adjacent nodes. Do this by pinging according to poll()
 	            //Create new node objects and add them to nodeMap
 	            int i = cur_node.getX();
 	            int j = cur_node.getY();
 	            String temp_str = null;
+	            Point temp_point= null;
 	            
                 for (int horz = i - 1; horz < i + 2; horz++) {
                     for (int vert = j - 1; vert < j + 2; vert++) {
                         if (inBounds(horz, vert)
                                 && (i != horz || j != vert)) {
-                            if(known.containsKey(new Point(horz,vert))){
-                                temp_str = known.get(new Point(horz,vert));
+                            temp_point = new Point(horz,vert);
+                            if(known.containsKey(temp_point)){
+                                temp_str = known.get(temp_point);
                             }else{
-                                temp_str = poll(new Point(horz,vert));
+                                temp_str = poll(temp_point);
                             }
-                            //TODO: If temp_str is valid, make a node for it and add it to nodeMap
-                            //Add if is O and not in queue and not visited
-                            //update cost and prev
-                            //If in queue, update if less
+                            //If temp_str is valid, make a node for it and add it to nodeMap
+                            if(!temp_str.equals("X")){
+                                if(!nodeMap.containsKey(temp_point)){
+                                nodeMap.put(temp_point, new Node(horz,vert,calcChebyshev(horz,vert)));
+                                }
+                                //Add to queue if is O and not in queue and not visited
+                                //update cost and prev
+                                if(!q.contains(nodeMap.get(temp_point))){
+                                    if(!nodeMap.get(temp_point).getVisited()){
+                                    nodeMap.get(temp_point).setPastCost(cur_node.getPastCost() + 1);
+                                    nodeMap.get(temp_point).setPrevNode(cur_node);
+                                    q.add(nodeMap.get(temp_point));
+                                    }
+                                }else{//If in queue, update if less
+                                    if((cur_node.getPastCost() + 1 + calcChebyshev(temp_point.x,temp_point.y)) < nodeMap.get(temp_point).getCost()){
+                                        q.remove(nodeMap.get(temp_point));
+                                        nodeMap.get(temp_point).setPastCost(cur_node.getPastCost() + 1);
+                                        nodeMap.get(temp_point).setPrevNode(cur_node);
+                                        q.add(nodeMap.get(temp_point));
+                                    }
+                                }
+                            }
+
                         }
                     }
                 }
@@ -170,6 +202,31 @@ public class MyRobotClassUncertain1 extends Robot {
 	        }
 
 	    }
+	    //Check if we ended because we go to final
+	    //If so, move to final
+	    //If not, print error.
+	       if(cur_node.getX() != destx || cur_node.getY() != desty){
+	            System.out.println("No path could be found!");
+	        }else{
+	            //System.out.println("Path Distance: " + dist);
+	            //System.out.print("Finish: ");
+	            
+	            //TODO:The PrevNode values are somehow getting into a loop. This isn't the fault of the code here, it's above. Still, needs a fix
+	            while(new Point(cur_node.getPrevNode().getX(),cur_node.getPrevNode().getY()) != super.getPosition()){
+	                System.out.println(cur_node);
+	                System.out.println(super.getPosition());
+	                //System.out.println(tmp);
+	                moveList.add(0,cur_node);
+	               cur_node = cur_node.getPrevNode();
+	            }
+	            //System.out.println("Start: " + tmp);
+	            
+	            for(int i = 0; i < moveList.size(); i++){
+	               temp_p = new Point(moveList.get(i).getX(),moveList.get(i).getY());
+	               super.move(temp_p);
+	        }
+	        }
+	    
 	    System.out.println("DONE");
 	}
 
